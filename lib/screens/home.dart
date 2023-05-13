134a9b3ac7a0b636044d1dc36e350044846cb5a4
Hriday Keswani <hridaykeswani@gmail.com>
2023-05-13 23:30:18 +0530

    implement bottom naviagation bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:pallifind/screens/faq.dart';
+import 'package:pallifind/screens/search.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Faq(),
+    Search(),
+    Text('Favorites'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +25,32 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Pallifind'),
         centerTitle: true,
       ),
-      body: Column(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'About Us',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
     );
   }
 }
